3159e40ef673e2ab413c7a4ee44cccf748bfef46
ichthoth <shalomtata334@gmail.com>
2024-05-16 05:15:08 +0100

    fixed 401 and 404 error

--- cobain.py
+++ cobain.py
@@ -12,18 +12,18 @@
 
 import requests
 import pandas as pd
-from dotenv import load_dotenv
+import pprint as pp
 
-url = "https://public-api.birdeye.so/defi/price?address=So11111111111111111111111111111111111111112"
-header = {"X-API-KEY": "e3194f932e1846429558b16dc3ef19b4"}
+url = "https://public-api.birdeye.so/defi/price?address=8vCAUbxejdtaxn6jnX5uaQTyTZLmXALg9u1bvFCAjtx7"
+header = {"x-chain":"solana","X-API-KEY": "e3194f932e1846429558b16dc3ef19b4"}
 
 
 
 response = requests.get(url,headers=header)
 
 if response.status_code == 200:
-    data = response.json()['data']
-
+    data = response.json()
+    pp.pprint(data)
     #creating a dataframe
     dataframe = pd.DataFrame(data)
 
@@ -31,6 +31,7 @@ if response.status_code == 200:
     csv_fp = 'cobain.csv'
     dataframe.to_csv(csv_fp,index=False)
     print("Data Saved to {csv_fp}")
+    #print(dataframe)
 else:
     print("failed to retrieve data",response.status_code)
 
